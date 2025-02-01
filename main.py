2672615002e01b4ba42384a310d23d9e87e3a06b
rpuri19 <ruchipuri19@gmail.com>
2025-02-02 00:31:27 +0100

    promotions added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import products
 import store
+import promotions
 
 def menu_display():
     """
@@ -25,6 +26,16 @@ def start():
                     products.NonStockedProduct("Windows License", price=125),
                     products.LimitedProduct("Shipping", price=10, quantity=250, maximum=1)
                     ]
+
+    # Create promotion catalog
+    second_half_price = promotions.SecondHalfPrice("Second Half price!")
+    third_one_free = promotions.ThirdOneFree("Third One Free!")
+    thirty_percent = promotions.PercentDiscount("30% off!", percent=30)
+
+    # Add promotions to products
+    product_list[0].set_promotion(second_half_price)
+    product_list[1].set_promotion(third_one_free)
+    product_list[3].set_promotion(thirty_percent)
 
     best_buy = store.Store(product_list)  # Initialize store with list of products
 
---
+++ promotions.py
@@ -0,0 +1,47 @@
+from abc import abstractmethod
+
+
+class Promotion:
+    def __init__(self, name: str):
+        self.name = name
+
+    @abstractmethod
+    def apply_promotion(self, product, quantity) -> float:
+        pass
+
+class PercentDiscount(Promotion):
+    def __init__(self, discount_name, percent):
+        super().__init__(discount_name)
+        self.percent = percent
+
+    def apply_promotion(self, product, quantity) -> float:
+        new_price = product.price * quantity * (1 - product.discount / 100)
+        return new_price
+
+class SecondHalfPrice:
+    def __init__(self, name):
+        super().__init__(name)
+
+    def apply_promotion(self, product, quantity) -> float:
+        if quantity < 2:
+            total_price = product.price * quantity
+            return total_price
+        full_price_items = quantity // 2
+        half_price_items = quantity - full_price_items
+
+        total_price = full_price_items * product.price + half_price_items * (0.5 * product.price)
+        return total_price
+
+class ThirdOneFree(Promotion):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def apply_promotion(self, product, quantity) -> float:
+        if quantity < 3:
+            total_price = product.price * quantity
+            return total_price
+
+        total_price = product.price * quantity * 2/3
+        return int(total_price)
+
+
